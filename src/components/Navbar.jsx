750cbf55630e072fda8a63310125796d39c22cf4
Harry EMARH <emarh.harry.code@gmail.com>
2025-04-29 23:37:59 +0200

    Navbar 2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
       <div className="text-2xl font-bold text-green-600">JOB STOCK</div>
       <ul className="hidden md:flex space-x-6 text-gray-700 font-medium">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">For Candidate</a></li>
-        <li><a href="#">For Employer</a></li>
-        <li><a href="#">Pages</a></li>
-        <li><a href="#">Help</a></li>
+        <li><Link href="#">Home</Link></li>
+        <li><Link href="#">For Candidate</Link></li>
+        <li><Link href="#">For Employer</Link></li>
+        <li><Link href="#">Pages</Link></li>
+        <li><Link href="#">Help</Link></li>
       </ul>
       <div className="space-x-4">
         <button className="text-sm text-green-600">Sign In</button>
